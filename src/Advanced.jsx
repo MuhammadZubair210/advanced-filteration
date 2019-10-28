8bdb3bff280c7d888b9116ce3658a78e6906007f
MuhammadZubair210 <mzubairabdullah21000@gmail.com>
2019-10-28 18:44:25 +0500

    .

--- src/Advanced.jsx
+++ src/Advanced.jsx
@@ -22,7 +22,7 @@ export default function DisplayAdvanced() {
           <Typography>Choose AMI</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-        </ExpansionPanelDetails>
+          </ExpansionPanelDetails>
       </ExpansionPanel>
 
       <ExpansionPanel>
